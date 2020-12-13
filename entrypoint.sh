3fe02eb8f74cba56966ea63532354e99f5b23cb1
rickstaa <rick.staa@outlook.com>
2020-12-13 12:30:18 +0100

    :sparkles: Adds reviewdog ci actions, testdata and removes github-pr-review warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,15 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
-  # erroformat: https://git.io/JeGMU
-  echo "ERROR: github-pr-review unsupported (for now)"
-  exit 1
-else
-  # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
-  remark --frail --quiet --use=remark-preset-lint-recommended . 2>&1 |
-    reviewdog -f=remark-lint -name="remark-lint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}" -tee
-fi
+# if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
+#   # erroformat: https://git.io/JeGMU
+#   echo "ERROR: github-pr-review unsupported (for now)"
+#   exit 1
+# else
+#   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
+#   remark --frail --quiet --use=remark-preset-lint-recommended . 2>&1 |
+#     reviewdog -f=remark-lint -name="remark-lint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}" -tee
+# fi
+
+remark --frail --quiet --use=remark-preset-lint-recommended . 2>&1 |
+  reviewdog -f=remark-lint -name="remark-lint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}" -tee
