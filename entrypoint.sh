97ce3894e2d34ef9618d808b8caa92507ec06700
rickstaa <rick.staa@outlook.com>
2020-12-14 09:21:50 +0100

    :fire: Removes github-pr-review warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,6 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
-  # erroformat: https://git.io/JeGMU
-  echo "ERROR: github-pr-review unsupported (for now)"
-  exit 1
-else
-  # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
-  remark --frail --quiet --use=remark-preset-lint-recommended . 2>&1 |
-    reviewdog -f=remark-lint -name="remark-lint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}" -tee
-fi
-
 remark --frail --quiet --use=remark-preset-lint-recommended . ${INPUT_REMARK_FLAGS} 2>&1 |
   sed 's/\x1b\[[0-9;]*m//g' | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
   reviewdog -f=remark-lint \
