79be8644e41e29fe98a118be1895057864ef073a
rickstaa <rick.staa@outlook.com>
2020-12-14 08:46:56 +0100

    :white_check_mark: Tests out new errorformat

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,12 +15,27 @@ else
     reviewdog -f=remark-lint -name="remark-lint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}" -tee
 fi
 
-remark --frail --quiet --use=remark-preset-lint-recommended . 2>&1 |
-  sed 's/\x1b\[[0-9;]*m//g' | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
-  reviewdog -f=remark-lint \
-  -name="${INPUT_TOOL_NAME}" \
-  -reporter="${INPUT_REPORTER:-github-pr-check}"\
-  -filter-mode="${INPUT_FILTER_MODE}" \
-  -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-  -level="${INPUT_LEVEL}" -tee \
-  ${INPUT_REVIEWDOG_FLAGS}
+# remark --frail --quiet --use=remark-preset-lint-recommended . 2>&1 |
+#   sed 's/\x1b\[[0-9;]*m//g' | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
+#   reviewdog -f=remark-lint \
+#   -name="${INPUT_TOOL_NAME}" \
+#   -reporter="${INPUT_REPORTER:-github-pr-check}"\
+#   -filter-mode="${INPUT_FILTER_MODE}" \
+#   -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+#   -level="${INPUT_LEVEL}" -tee \
+#   ${INPUT_REVIEWDOG_FLAGS}
+
+# Test out new format
+remark --frail --quiet --use=remark-preset-lint-recommended . 2>&1
+  | sed 's/\x1b\[[0-9;]*m//g'
+  | reviewdog \
+      -efm='%-P%f' \
+      -efm='%#%l:%c %# %trror  %m' \
+      -efm='%#%l:%c %# %tarning  %m' \
+      -efm='%-Q' \
+      -name="${INPUT_TOOL_NAME}" \
+      -reporter="${INPUT_REPORTER:-github-pr-check}"\
+      -filter-mode="${INPUT_FILTER_MODE}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -level="${INPUT_LEVEL}" -tee \
+      ${INPUT_REVIEWDOG_FLAGS}
